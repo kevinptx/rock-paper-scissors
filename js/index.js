4a671d99e14b8bc9178303fdc81bdf51800d94e4
Kevin Peery <kevin.peery@gmail.com>
2018-12-01 10:53:53 -0700

    update javascript logic

--- js/index.js
+++ js/index.js
@@ -1,23 +1,24 @@
-let rockDiv = document.getElementById("rock");
-let paperDiv = document.getElementById("paper");
-let scissorsDiv = document.getElementById("scissors");
-let userChoice = document.getElementById("user-choice");
-let computerChoice = document.getElementById("computer-choice");
-let result = document.getElementById("result");
-let choiceArray = ["rock", "paper", "scissors"];
+const rockDiv = document.getElementById("rock");
+const paperDiv = document.getElementById("paper");
+const scissorsDiv = document.getElementById("scissors");
+const userChoice = document.getElementById("user-choice");
+const computerChoice = document.getElementById("computer-choice");
+const result = document.getElementById("result");
+const choiceArray = ["rock", "paper", "scissors"];
 let random = "";
 
 function handleComputerChoice() {
   random = choiceArray[Math.floor(Math.random() * choiceArray.length)];
-}
-
-rockDiv.addEventListener("click", function() {
-  userChoice.innerHTML = "rock";
-  handleComputerChoice();
   computerChoice.innerHTML = random;
-  let computerString = computerChoice.textContent.replace(/^\w/, c =>
+  computerString = computerChoice.textContent.replace(/^\w/, c =>
     c.toUpperCase()
   );
+}
+
+
+rockDiv.addEventListener("click", function () {
+  userChoice.innerHTML = "rock";
+  handleComputerChoice();
   let userString = userChoice.textContent.replace(/^\w/, c => c.toUpperCase());
   if (random == "rock") {
     result.innerHTML = `It's a tie! ${userString} equals ${computerString}.`;
@@ -28,30 +29,22 @@ rockDiv.addEventListener("click", function() {
   }
 });
 
-paperDiv.addEventListener("click", function() {
+paperDiv.addEventListener("click", function () {
   userChoice.innerHTML = "paper";
   handleComputerChoice();
-  computerChoice.innerHTML = random;
-  let computerString = computerChoice.textContent.replace(/^\w/, c =>
-    c.toUpperCase()
-  );
   let userString = userChoice.textContent.replace(/^\w/, c => c.toUpperCase());
   if (random == "paper") {
-    result.innerHTML = `It's a tie! ${computerString} equals ${userString}.`;
+    result.innerHTML = `It's a tie! ${userString} equals ${computerString}.`;
   } else if (random == "rock") {
     result.innerHTML = `You Win! ${userString} beats ${computerString}.`;
   } else if (random == "scissors") {
-    result.innerHTML = `You Lose! ${computerString} beats ${computerString}.`;
+    result.innerHTML = `You Lose! ${computerString} beats ${userString}.`;
   }
 });
 
-scissorsDiv.addEventListener("click", function() {
+scissorsDiv.addEventListener("click", function () {
   userChoice.innerHTML = "scissors";
   handleComputerChoice();
-  computerChoice.innerHTML = random;
-  let computerString = computerChoice.textContent.replace(/^\w/, c =>
-    c.toUpperCase()
-  );
   let userString = userChoice.textContent.replace(/^\w/, c => c.toUpperCase());
   if (random == "scissors") {
     result.innerHTML = `It's a tie! ${userString} equals ${computerString}.`;
